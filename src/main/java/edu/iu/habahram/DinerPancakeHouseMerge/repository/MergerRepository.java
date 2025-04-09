5793a09fe58b3524ca60c2cd5d0ec966ad857aab
manandi-iu <44542974+ManishReddyN@users.noreply.github.com>
2025-04-09 15:15:16 -0400

    Remove unused import statement from MergerRepository

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/MergerRepository.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/MergerRepository.java
@@ -1,7 +1,6 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.repository;
 
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
